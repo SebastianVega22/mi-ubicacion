59e4aac59d492a9cf84c0f619a5def6f7931c810
SebastianVega22 <116905769+SebastianVega22@users.noreply.github.com>
2024-09-03 18:59:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var map;
-var targetLat = 4.7399325; // Latitud de la nueva ubicación específica (Cra. 145a #132b-28, Bogotá)
-var targetLon = -74.1303559; // Longitud de la nueva ubicación específica
+var targetLat = 4.712562; // Latitud de la nueva ubicación específica (Cra. 145a #132b-28, Bogotá)
+var targetLon = -74.196338; // Longitud de la nueva ubicación específica
 var marginOfError = 0.005; // Margen de error en grados (~555 metros)
 
 document.getElementById('idForm').addEventListener('submit', function(event) {
